45f9b6be8ed77c8d0ba78ca851d9a95229e914a5
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2020-11-24 13:15:14 +0530

    task completed and task checked function added

--- dashboard.js
+++ dashboard.js
@@ -1,25 +1,41 @@
-window.addEventListener("load",function(){
-    var add_task=document.getElementById("add-from-taskbar");
-    add_task.addEventListener("click",add_from_taskbar);
+window.addEventListener("load", function () {
+  var add_task = document.getElementById("add-from-taskbar");
+  add_task.addEventListener("click", add_from_taskbar);
 });
-var countt=0;
-function add_from_taskbar(){
+var countt = 0;
+var countcomp = 0;
+function add_from_taskbar() {
+  var task_input = document.getElementById("task-bar").value;
+  if (task_input != "") {
+    var label = document.createElement("label");
+    label.textContent = task_input;
+    var br = document.createElement("br");
+    var checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    var task_count = document.getElementById("count");
+    countt++;
+    task_count.textContent = countt;
+    var del = document.createElement("button");
+    del.textContent = "Delete";
+    del.setAttribute("class", "del");
+    var pertask = document.createElement("div");
+    pertask.append(checkbox, label, del);
+    var container = document.getElementById("task-container");
+    container.appendChild(pertask);
+    del.addEventListener("click", deltask);
+  }
+}
+function deltask() {
+  if (event.target.previousSibling.previousSibling.checked==true) {
+    countcomp++;
+    var comp = document.getElementById("completed");
+    comp.textContent = countcomp;
+    event.target.parentNode.remove();
    
-    var task_input=document.getElementById("task-bar").value;
-    if(task_input!=""){
-        var label=document.createElement("label");
-        label.textContent=task_input;
-        var br=document.createElement("br");
-        var checkbox=document.createElement("input");
-        checkbox.setAttribute("type","checkbox");
-        var task_count=document.getElementById("count");
-        countt++;
-        task_count.textContent=countt;
-        var del=document.createElement("button");
-        del.textContent = "Delete";
-        del.setAttribute("class","del");
-        var container=document.getElementById("task-container");
-        container.append(checkbox,label,del,br);
-    }
-  
+  } else {
+    event.target.parentNode.remove();
+  }
+  var task_count = document.getElementById("count");
+  countt--;
+  task_count.textContent = countt;
 }
