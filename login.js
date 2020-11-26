0a596e064352ea5a11df21e7170e0c618749d7a4
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2020-11-26 09:59:09 +0530

    minor changes in login page

--- login.js
+++ login.js
@@ -14,25 +14,30 @@ function handleData(event){
     var form= new FormData(document.getElementById("form"))
     let userName=form.get('username');
     let password=form.get('password');
-    
-    var target = JSON.parse(localStorage.getItem('users'))
-    console.log(target)
-    for(var i=0;i<target.length;i++){
-        console.log(target[i].name)
-        if(userName == target[i].name && password == target[i].password) {
-            // console.log(email + 'login successful')
-            location.href = "dashboard.html"
+    if(localStorage.getItem('users')){
+        var target = JSON.parse(localStorage.getItem('users'))
+        console.log(target)
+        for(var i=0;i<target.length;i++){
+            console.log(target[i].name)
+            if(userName == target[i].name && password == target[i].password) {
+                // console.log(email + 'login successful')
+                location.href = "dashboard.html"
+            }
+            else if(userName != target[i].name && password != target[i].password){
+                handleResponse(`Account doesn't exists`)
+            }
+            else if(userName == target[i].name || password != target[i].password) {
+                handleResponse('Wrong Password')
+                console.log("wrong user")
+            
+            }
+            
         }
-        else if(userName != target[i].name && password != target[i].password){
-            handleResponse(`Account doesn't exists`)
-        }
-        else if(userName == target[i].name || password != target[i].password) {
-            handleResponse('Wrong Password')
-            console.log("wrong user")
-        
-        }
-        
     }
+  else{
+    localStorage.setItem("users","[]");
+    alert("Account Doesn't Exist ");
+  }
 }
 
 function handleResponse(str){
